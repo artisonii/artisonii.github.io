6bf13faae8f9e927e5662654eebe018b7f861d37
artisonii <artisoni2615@gmail.com>
2023-12-07 11:59:57 +0530

    github start

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Navbar = () => {
+    return (
+        <div id="nav-menu">Navbar</div>
+    )
+}
+
+export default Navbar
---
+++ src/components/SkillSection/Skill.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Skill = () => {
+    return (
+        <div id='skills'>Skill</div>
+    )
+}
+
+export default Skill
